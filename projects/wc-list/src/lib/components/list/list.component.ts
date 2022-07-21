86bf7f5021de6a674ee9f6372d5072d8e9de9dce
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-07-22 00:34:31 +0400

    feat: added selection mode to the wc list

--- projects/wc-list/src/lib/components/list/list.component.ts
+++ projects/wc-list/src/lib/components/list/list.component.ts
@@ -1,23 +1,58 @@
-import {Component, ContentChildren} from '@angular/core';
+import {Component, ContentChildren, Input, OnInit} from '@angular/core';
 import '@ui5/webcomponents/dist/List.js';
 import {ListItemComponent} from "../list-item/list-item.component";
+import {BooleanInput, coerceBooleanProperty} from "@angular/cdk/coercion";
+// @ts-ignore
+import ListMode from "@ui5/webcomponents/dist/types/ListMode.js";
 
 @Component({
   selector: 'ul[wc-list]',
   templateUrl: './list.component.html',
   styles: [
     `
-        :host {
-          padding: 0;
-          margin: 0
-        }
+      :host {
+        padding: 0;
+        margin: 0
+      }
     `
   ]
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  @Input()
+  get multiple(): boolean {
+    return this._multiple;
+  }
+
+  set multiple(value: BooleanInput) {
+    this._multiple = coerceBooleanProperty(value);
+  }
+
+  @Input()
+  get selectable(): boolean {
+    return this._selectable;
+  }
+
+  set selectable(value: BooleanInput) {
+    this._selectable = coerceBooleanProperty(value);
+  }
+
+  get listMode() {
+    if (!this._selectable) {
+      return ListMode.None;
+    }
+    return this._multiple ? ListMode.MultiSelect : ListMode.SingleSelect;
+  }
+
   @ContentChildren(ListItemComponent)
   listItems!: ListItemComponent[];
 
+  private _multiple: boolean = false;
+  private _selectable: boolean = false;
+
   constructor() {
+  }
+
+  ngOnInit(): void {
+    console.log(this.listMode)
   }
 }
