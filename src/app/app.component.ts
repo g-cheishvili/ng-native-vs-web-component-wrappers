1e677d70fcd014cd391f36354ec5966e1869da02
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-07-22 02:38:55 +0400

    chore: added just simple representation of parsed data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import ngListDemoStats from 'dist/ng-list-demo/stats.json';
+import ngSelectableListDemoStats from 'dist/ng-selectable-list-demo/stats.json';
+import wcListDemoStats from 'dist/wc-list-demo/stats.json';
+import wcSelectableListDemoStats from 'dist/wc-selectable-list-demo/stats.json';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-native-components-vs-web-components';
+  stats: Record<string, any> = {
+    ngListDemoStats,
+    ngSelectableListDemoStats,
+    wcListDemoStats,
+    wcSelectableListDemoStats
+  };
+  mainFileSizes: string[] = [];
+
+  constructor() {
+    Object.keys(this.stats).forEach((statsFileName: string) => {
+      const statsFile = this.stats[statsFileName];
+      const main = statsFile.chunks.map((chunk: any) => ({
+        name: chunk.names[0],
+        size: Math.round(chunk.size / 1024)
+      })).find((c: any) => c.name === 'main');
+      this.mainFileSizes.push(`size of ${statsFileName}'s main.js = ${main.size} kb`);
+    });
+  }
 }
