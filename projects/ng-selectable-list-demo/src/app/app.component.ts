e4d2a98a326197e3f389d72c74b7f82078b8ed3c
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-07-21 16:36:58 +0400

    chore: set up initial selectable list application

--- projects/ng-selectable-list-demo/src/app/app.component.ts
+++ projects/ng-selectable-list-demo/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-selectable-list-demo';
+  items = new Array(10).fill(0).map((_, i) => `Item ${i}`);
 }
--- projects/ng-selectable-list-demo/src/app/app.module.ts
+++ projects/ng-selectable-list-demo/src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NgListModule} from "ng-list";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
